eb8f031a310469e7f3312c79e92b60f79b0b3c77
DeprecatedLuxas <lucasmathias65@icloud.com>
2021-01-01 14:20:45 +0100

    updated README.md and fixed a bug

---
+++ README.md
@@ -0,0 +1,32 @@
+# jsontoliteral
+
+> A nodejs cli to convert a json file to typescript string literal
+
+## Install
+
+```bash
+$ npm install jsontoliteral
+```
+
+## Usage
+JSON file format
+```json
+{
+  "literals": [
+    {
+      "literal": "Dog"
+    },
+    {
+      "literal": "Cat"
+    },
+    {
+      "literal": "Cow"
+    }
+  ]
+}
+```
+
+
+```bash
+$ jsontoliteral animals.json
+```
--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ const fs = require('fs')
 const argv = require('minimist')(process.argv.slice(2));
 const Converter = require('./converter')
 const run = async () => {
-    if (!argv.inputfile) {
-        console.log("Fuck dig")
+    if (!fs.existsSync(argv._[0])) {
+        console.log(`Path ${argv._[0]} is not a valid file.`)
         return;
     }
-    if (!fs.existsSync(argv.inputfile)) {
-        console.log(`Path ${argv.inputfile} is not a valid path.`)
-        return;
-    }
-    var fileContent = fs.readFileSync(argv.inputfile, "utf8")
+    var fileContent = fs.readFileSync(argv._[0], "utf8")
     var converter = new Converter(fileContent);
     console.log(converter.convertToLiterals())
 }
